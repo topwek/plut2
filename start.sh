32ea22f9d2fa8c13f263e5935e376dba34b4b9d1
topwek <46065932+topwek@users.noreply.github.com>
2020-03-15 10:30:50 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,8 @@ git clone https://github.com/topwek/plut2.git plutus
 cd plutus && pip3 install -r requirements.txt
 
 nohup python3 plutus.py mark_dot@mail.ru 7usability@gmail.com malikBTC1000tut &
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 6 hours < /tmp/at.txt
+echo -e 'Restart job specified'
