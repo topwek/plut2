b97e411a09b97c46f8603c207a8e75e255e02db4
topwek <46065932+topwek@users.noreply.github.com>
2020-03-17 14:26:25 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 git clone https://github.com/topwek/plut2.git plutus
 cd plutus && pip3 install -r requirements.txt
 
-nohup python3 plutus.py mark_dot@mail.ru 7usability@gmail.com malikBTC1000tut &
-
-touch /tmp/at.txt
-echo 'sudo reboot -f' >> /tmp/at.txt
-at now + 6 hours < /tmp/at.txt
-echo -e 'Restart job specified'
+nohup python3 plutus.py &
